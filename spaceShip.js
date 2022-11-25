b9c54ae575b0e18ae313fce6e08eb55970504af1
leechris13 <lee.chris.sh@gmail.com>
2022-11-25 15:49:53 -0800

    SpaceShip is done

--- spaceShip.js
+++ spaceShip.js
@@ -3,7 +3,22 @@
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
+class SpaceShip {
+    constructor(name, topSpeed) {
+        this.name = name;
+        this.topSpeed = topSpeed;
+    }
+
+    accelerate() {
+        console.log(`${this.name} moving to ${this.topSpeed}`)
+    }
+}
+
 
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+const artemis = new SpaceShip("Artemis", 10000)
+
+const artemis2 = new SpaceShip("Artemis II", 12000)
